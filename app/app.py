4358aff1b7d8a578f60eb8cb7c81a9c825208a00
dmaproz <91570185+dmaproz@users.noreply.github.com>
2025-12-20 15:41:52 -0400

    added setup python

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/app/v1/details', methods=['GET'])
 def details():
     return jsonify({
-        'message': "We are going forward! Some stuffs extra!.....",
+        'message': "We are going forward! Some stuffs extra!......",
         'hostname': socket.gethostname(),
         'time': str(datetime.datetime.now()),
     })
